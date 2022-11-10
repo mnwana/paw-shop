ab74e952353b65a00e33d017d79cb6426eb85ddf
mnwana <mariellenwana@gmail.com>
2022-11-10 00:16:35 -0500

    add comments to see if comments should be included in post schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,41 +3,44 @@ const { Comment } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-      
+  Query: {},
+  Mutation: {
+    addComment: async (parent, args, context) => {
+      if (context.user) {
+        const comment = await Comment.create({
+          ...args,
+          username: context.user.username,
+        });
+        // Todo: should this be post instead?
+        // await Post.findByIdAndUpdate(
+        await User.findByIdAndUpdate(
+          // { _id: postId },
+          { _id: context.user._id },
+          { $push: { comments: comment._id } },
+          { new: true }
+        );
 
-    },
-    Mutation: {
-        addComment: async (parent, args, context) => {
-            if (context.user) {
-              const comment = await Comment.create({ ...args, username: context.user.username });
-      
-              await User.findByIdAndUpdate(
-                { _id: context.user._id },
-                { $push: { comments: comment._id } },
-                { new: true }
-              );
-      
-              return thought;
-            }
-      
-            throw new AuthenticationError('You need to be logged in!');
-          },
-          addReply: async (parent, { commentId, replyBody }, context) => {
-            if (context.user) {
-              const updatedComment = await Comment.findOneAndUpdate(
-                { _id: commentId },
-                { $push: { replies: { replyBody, username: context.user.username } } },
-                { new: true, runValidators: true }
-              );
-      
-              return updatedComment;
-            }
-      
-            throw new AuthenticationError('You need to be logged in!');
-          },
+        return thought;
       }
-  };
-  
-  module.exports = resolvers;
-  
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    addReply: async (parent, { commentId, replyBody }, context) => {
+      if (context.user) {
+        const updatedComment = await Comment.findOneAndUpdate(
+          { _id: commentId },
+          {
+            $push: { replies: { replyBody, username: context.user.username } },
+          },
+          { new: true, runValidators: true }
+        );
+
+        return updatedComment;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
