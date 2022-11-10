e20b5f5f575475041f6d2c6aa13619963885adc8
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-10 13:02:19 -0500

    add `useState` for active nav link

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,51 +1,72 @@
 
 // IMPORTS
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
+
+import './index.css';
+
 
 
 // COMPONENT
 export default function Navbar(){
+    const [activePage, setActivePage] = useState('Listings');
+    function handleNavClick({target}){
+        const page = target.getAttribute('page');
+        if (page)
+            setActivePage(page);
+    }
+
     const testerLoggedIn = true;  // UPDATE LATER to pull from client-side `utils/auth.js`
 
     const items = [
         {
             name: 'Listings',
             link: '/',
-            reqsLogin: null
+            reqsLogin: null,
+            isPage: true
         },
         {
             name: 'Post an item',
             link: '/post-item',
-            reqsLogin: true
+            reqsLogin: true,
+            isPage: true
         },
         {
             name: 'My account',
             link: '/account',
-            reqsLogin: true
+            reqsLogin: true,
+            isPage: true
         },
         {
             name: 'Sign up / log in',
             link: null,  // UPDATE LATER to activate modal
-            reqsLogin: false
+            reqsLogin: false,
+            isPage: false
         },
         {
             name: 'Log out',
             link: null,  // UPDATE LATER to log user out
-            reqsLogin: true
+            reqsLogin: true,
+            isPage: false
         }
     ];
 
     return (
         <header>
-            <h1>Site title</h1>
+            <h1>
+                <Link to='/' page='Home' onClick={handleNavClick}>Site title</Link>
+            </h1>
 
             <nav>
                 <ul>
                     {items
                         .filter(item => item.reqsLogin === null || item.reqsLogin === testerLoggedIn)
                         .map(item =>
-                            <li key={item.name}>
-                                <Link to={item.link}>
+                            <li
+                                key={item.name}
+                                className={`${activePage === item.name ? 'active' : ''}`}
+                            >
+                                <Link to={item.link} page={item.isPage ? item.name : ''} onClick={handleNavClick}>
                                     {item.name}
                                 </Link>
                             </li>
