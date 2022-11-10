223dab37150b244b6d886e39984584165b41faec
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-10 15:24:43 -0500

    update formatting of joinAuthors helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -3,21 +3,18 @@ export function validateEmail(email){
     return !!email.match(/^[a-z0-9._]{3,}@([a-z0-9-]+\.)+?[a-z]{2,3}$/i);
 }
 
-export function joinAuthors(authors){
-    function authorLink(author, charsAfter = ''){
-        return (
-            <span key={author.name}>
-                <a href={`https://github.com/${author.github}`} target='_blank'>{author.name}</a>
-                {charsAfter}
-            </span>
-        );
-    }
 
-    return (
-        <>
+export function joinAuthors(authors){
+    const authorLink = (author, charsAfter = '') =>
+        <span key={author.name}>
+            <a href={`https://github.com/${author.github}`} target='_blank'>{author.name}</a>
+            {charsAfter}
+        </span>
+    ;
+
+    return <>
         {authors.slice(0, authors.length - 2).map(author => authorLink(author, ', '))}
         {authorLink(authors[authors.length - 2], ' + ')}
         {authorLink(authors[authors.length - 1])}
-        </>
-    );
+    </>;
 }
