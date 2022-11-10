5f7cd70d4bd216b8b2b93b956d949f89ab73691f
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-10 10:42:11 -0500

    update helper functions

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,8 +1,4 @@
 
-export function capitalizeFirstLetter(word){
-    return word.substring(0, 1).toUpperCase() + word.substring(1);
-}
-
 export function validateEmail(email){
-    return email.match(/^[a-z0-9._]{3,}@([a-z0-9-]+\.)+?[a-z]{2,3}$/i) ? true : false;
+    return !!email.match(/^[a-z0-9._]{3,}@([a-z0-9-]+\.)+?[a-z]{2,3}$/i);
 }
