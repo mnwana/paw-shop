4303706bc7b3e446188d2674d58effe8a78a49ae
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-12 12:00:42 -0500

    add Clear All and Apply buttons to FilterPanel

--- client/src/components/FilterPanel/index.js
+++ client/src/components/FilterPanel/index.js
@@ -1,5 +1,6 @@
 
 // IMPORT
+import $ from 'jquery';
 import FilterGroup from '../FilterGroup';
 import {useStoreContext} from '../../utils/GlobalState';
 
@@ -8,8 +9,34 @@ import {useStoreContext} from '../../utils/GlobalState';
 export default function FilterPanel(){
     let [{filterState}] = useStoreContext();
 
+    
+    function handleClearAll(){
+        $('.select-none-btn').trigger('click');
+    }
+    
+
+    function handleApply(){  // UPDATE LATER
+        const output = ['Getting ready to apply this filter state:', ''];
+        let atLeastOneChecked = false;
+
+        for (const group of filterState)
+            for (const element of group.elements){
+                if (!atLeastOneChecked && element.checked)
+                    atLeastOneChecked = true;
+                output.push(`Name: ${element.name}   ||   Checked: ${element.checked}`);
+            }
+
+        alert(atLeastOneChecked ? output.join('\n') : 'At least one filter field must be checked!');
+    }
+
+
     return (
         <div className="filter-panel">
+            <div className='global-filter-btns-wrapper'>
+                <button className='apply-btn btn btn-success' onClick={handleApply}>Apply</button>
+                <button className='clear-all-btn btn btn-info' onClick={handleClearAll}>Clear all</button>
+            </div>
+
             {filterState.map(({group, elements}) => <FilterGroup group={group} elements={elements} key={group}/>)}
         </div>
     );
