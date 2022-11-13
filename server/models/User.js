8a99e12b932e2276f403b627708b8fd68a869d84
mnwana <mariellenwana@gmail.com>
2022-11-13 14:52:24 -0500

    rebase from dev

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,41 @@
-const { Schema, model } = require('mongoose');
-const { enum } = require('sequelize/types');
-const dateFormat = require('../utils/dateFormat');
-const Watchlist = require('./Watchlist')
-const Post = require('./Post')
+const { Schema, model } = require("mongoose");
+const { enum } = require("sequelize/types");
+const dateFormat = require("../utils/dateFormat");
+const Watchlist = require("./Watchlist");
+const Post = require("./Post");
 
-const UserSchema = new Schema({
-    
+const UserSchema = new Schema(
+  {
     username: {
       type: String,
       required: true,
       minLength: 5,
       maxLength: 16,
-      trim: true
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       trim: true,
-      validate: [validateEmail, 'Email address is invalid']
+      validate: [validateEmail, "Email address is invalid"],
     },
     borough: {
-        type: String,
-        enum: ["Manhattan", "Brooklyn", "Queens", "Staten Island", "The Bronx"],
-        required: true,
-        trim: true
-      },
+      type: String,
+      enum: ["Manhattan", "Brooklyn", "Queens", "Staten Island", "The Bronx"],
+      required: true,
+      trim: true,
+    },
     posts: [postSchema],
-    watchlist: [WatchlistSchema]
-},
-{
+    watchlist: [WatchlistSchema],
+  },
+  {
     toJSON: {
-        virtuals: true, 
-    }, 
-    id: false
-  });
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
