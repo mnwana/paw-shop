e8660a1276e6a2ea4e2ae61927e53e02e432effb
mnwana <mariellenwana@gmail.com>
2022-11-13 14:59:51 -0500

    fix typo in watchlist

--- server/models/Watchlist.js
+++ server/models/Watchlist.js
@@ -2,16 +2,18 @@
 const { Schema, model } = require('mongoose');
 
 const WatchlistSchema = new Schema(
-    
+    {
     postId: {
       type: Schema.Types.ObjectId,
       ref: "Post"
     },
+    }
   {
     toJSON: {
     }, 
     id: false
-  });
+  }
+);
 
   Watchlist.virtual('watchedCount').get(function() {
     return this.postId.length;
