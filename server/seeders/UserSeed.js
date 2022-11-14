bdf6479fe1a7bc77c79d0b92d090bc491d42f92d
claudiayile <84867833+claudiayile@users.noreply.github.com>
2022-11-13 21:00:42 -0500

    Update UserSeed.js

--- server/seeders/UserSeed.js
+++ server/seeders/UserSeed.js
@@ -2,7 +2,8 @@
     {
         "username": "billybob",
         "email" : "billy@bob.com",
-        "borough" : "Brooklyn",
+        "borough" : "brooklyn",
+        "postID" : 2,
         "post" : [
             {
                 "postText" : "I have a cat stroller for a cat under 12 pounds. You can have it if I can borrow a power drill for one afternoon.",
@@ -24,10 +25,32 @@
         ]
     },
     {
+        "username": "larry",
+        "email" : "larryhasacookie@gmail.com",
+        "borough" : "the bronx",
+        "postID" : 3,
+        "post" : [
+            {
+                "postText" : "mother says i need to find a new home for the cat",
+                "comments" : [
+            {   "username" : "imogene",
+                    "commentBody" : "I'll take your cat, dm me on IG @imogene", 
+                    "replies" : [
+                        {
+                            "replyBody" : "great, just sent you a DM",
+                            "username" : "larry"
+                        },
+                    ]}
+                ]
+            }
+        ]
+    },
+    {
         "username": "cococat",
         "email" : "coco@cat.com",
-        "borough" : "Manhattan",
-        "post" : [
+        "borough" : "manhattan",
+        "postID" : 4,
+        "post" : [ 
             {
                 "postText" : "Meow Meow",
                 "comments" : [
@@ -40,9 +63,31 @@
         ]
     },
     {
+        "username": "ruffstuff",
+        "email" : "ruffstuff@loveme.com",
+        "borough" : "manhattan",
+        "postID" : 22,
+        "post" : [] 
+            }
+        ]
+    },
+        {
+        "username": "lovable",
+        "email" : "amiagod@loveme.com",
+        "borough" : "manhattan",
+        "postID" : 300,
+        "post" : [{
+                "postText" : "are we alone in the universe",
+                "comments" : []
+            }] 
+            }
+        ]
+    },
+    {
         "username": "bigboyfella",
         "email" : "bullfighter@riskystuff.com",
-        "borough" : "Manhattan",
+        "borough" : "manhattan",
+        "postID" : 18,
         "post" : [
             {
                 "postText" : "garbhj garble gabagaj garble garble uhuh",
@@ -50,10 +95,35 @@
             }
         ]
     },
+            {
+        "username": "gabagool",
+        "email" : "gabagool@gabagool.com",
+        "borough" : "manhattan",
+        "postID" : 108,
+        "post" : [
+            {
+                "postText" : "u r a dog",
+                "comments" : []
+            }
+        ]
+    },
+        {
+        "username": "angelxo",
+        "email" : "angels@gabagool.com",
+        "borough" : "brooklyn",
+        "postID" : 19,
+        "post" : [
+            {
+                "postText" : "does anybody have anything for cows?",
+                "comments" : []
+            }
+        ]
+    },
     {
         "username": "asdhposiifhuwl",
         "email" : "asldijw@rkldj.org",
-        "borough" : "The Bronx",
+        "borough" : "the bronx",
+        "postID" : 199,
         "post" : [
             {
                 "postText" : "garbhj garble gabagaj garble garble uhuh",
@@ -65,7 +135,8 @@
     {
         "username": "lanadelrey",
         "email" : "lana@delrey.com",
-        "borough" : "Brooklyn",
+        "borough" : "brooklyn",
+        "postID" : 90,
         "post" : [
             {
                 "postText" : "I am Lana Del Rey AMA",
@@ -107,7 +178,7 @@
     {
         "username": "psychic",
         "email" : "iseeu@canada.org",
-        "borough" : "Staten Island",
+        "borough" : "staten island",
         "post" : [
             {
                 "postText" : "i have some treats my cats hate",
@@ -134,8 +205,47 @@
                     "commentBody" : "meow meow meow", 
                     "replies" : []
             }, 
-                    
-                ]
+            {
+        "username": "monicavitti",
+        "email" : "vitti@canada.org",
+        "borough" : "manhattan",
+        "post" : [
+            {
+                "postText" : "i have some treats my cats hate",
+                "comments" : [
+                        {
+                            "replyBody" : "i'll eat em",
+                            "username" : "billybob"
+                        }
+                    ]
+            },   
+            ]
+            }, 
+        "username": "psychic",
+        "email" : "iseeu@canada.org",
+        "borough" : "staten island",
+        "post" : [
+            {
+                "postText" : "i have some treats my cats hate",
+                "comments" : [
+                    {   "username" : "billybob",
+                    "commentBody" : "aren't you a psychic?", 
+                    "replies" : [
+                        {
+                            "replyBody" : "my cat ate my crystal ball",
+                            "username" : "psychic"
+                        },
+                        {
+                            "replyBody" : "no way, that happened to my cousin",
+                            "username" : "billybob"
+                        }
+                    ]
+            },   
+            ]
+            },
+            {
+                "postText" : "is your fridge running?",
+                "comments" : []
             }
         ]
     },
@@ -168,6 +278,44 @@
                 ]
             }
         ]
-    }
-
+    }, 
+    {
+        "username": "instagram",
+        "email" : "instagram@yahoo.com",
+        "borough" : "Brooklyn",
+        "post" : [
+            {
+                "postText" : "I have a lego set.",
+                "comments" : [
+            {   "username" : "avrillavigne",
+                    "commentBody" : "I'd love your lego", 
+                    "replies" : [
+                        {
+                            "replyBody" : "that's so nice. I'll text you",
+                            "username" : "instagram"
+                        },
+                        {
+                            "replyBody" : "yup",
+                            "username" : "avrillavigne"
+                        }
+                    ]}
+                ]
+            }
+        ]
+    }, 
+        {
+        "username": "riley9",
+        "email" : "riley9@yahoo.com",
+        "borough" : "Brooklyn",
+        "post" : [
+            {
+                "postText" : "bing bong bing.",
+                "comments" : [
+            {   "username" : "riley8",
+                    "commentBody" : "bingbongBoom", 
+                    "replies" : []}
+                ]
+            }
+        ]
+    }, 
 ]
