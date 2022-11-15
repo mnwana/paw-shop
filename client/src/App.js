d498ad1aa672b4fc902b13ae6dcaeb33ec1793c7
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-15 10:08:48 -0500

    set up JWT + ApolloProvider on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom
 
 import {StoreProvider} from './utils/GlobalState';
 
+import {ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+import {localStorageTokenName} from './utils/auth';
+
 import Navbar from './components/Navbar';
 import LoginSignupModal from './components/LoginSignupModal';
 import Footer from './components/Footer';
@@ -20,28 +24,50 @@ import './App.css';
 
 
 
+// MIDDLEWARE
+const httpLink = createHttpLink({uri: '/graphql'});  // See package.json's `proxy` value: The development client will prefix the URL of all Apollo GraphQL server requests with the `proxy` value (for the production server, this will not happen, because client and server will reside at the same location)
+
+const authLink = setContext((_, {headers}) => { 
+    const token = localStorage.getItem(localStorageTokenName);
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
+
+
+
 // COMPONENT
 export default function App() {
     return (
-        <Router>
-            <StoreProvider>
-                <LoginSignupModal />  {/* UPDATE LATER to only render when logged in */}
+        <ApolloProvider client={client}>
+            <Router>
+                <StoreProvider>
+                    <LoginSignupModal />  {/* UPDATE LATER to only render when logged in */}
 
-                <Navbar />
-                
-                <main>
-                    <Routes>
-                        <Route path='/posts' element={<Posts />} />
-                        <Route path='/post-an-item' element={<PostAnItem />} />
-                        <Route path='/account' element={<MyAccount />} />
-                        <Route path='/post/:postId' element={<SinglePost />} />
-                        <Route path='/user/:username' element={<User />} />
-                        <Route path='*' element={<Navigate to='/posts' />} />
-                    </Routes>
-                </main>
-                
-                <Footer />
-            </StoreProvider>
-      </Router>
+                    <Navbar />
+                    
+                    <main>
+                        <Routes>
+                            <Route path='/posts' element={<Posts />} />
+                            <Route path='/post-an-item' element={<PostAnItem />} />
+                            <Route path='/account' element={<MyAccount />} />
+                            <Route path='/post/:postId' element={<SinglePost />} />
+                            <Route path='/user/:username' element={<User />} />
+                            <Route path='*' element={<Navigate to='/posts' />} />
+                        </Routes>
+                    </main>
+                    
+                    <Footer />
+                </StoreProvider>
+            </Router>
+        </ApolloProvider>
     );
 }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,33 +1,48 @@
-const jwt = require('jsonwebtoken');
 
-const secret = 'ourlittlesecrets!';
+// IMPORTS
+const jwt = require('jsonwebtoken');
+require('dot-env');
+
+
+// JWT SETTINGS
+const secret = process.env.JWT_SECRET || 'test secret phrase';
 const expiration = '2h';
 
+
+// EXPORTS
 module.exports = {
-    authMiddleware: function({ req }) {
+    authMiddleware({req}){
         let token = req.body.token || req.query.token || req.headers.authorization;
 
-        if (req.headers.authorization) {
+        if (req.headers.authorization)  // In the HTTP header, shave off the word 'Bearer' at the beginning of the value
             token = token
-            .split(' ')
-            .pop()
-            .trim();
-        }
+                .split(' ')
+                .pop()
+                .trim();
 
-        if (!token) {
+        if (!token)
             return req;
-        }
-        try {
-            const {data} = jwt.verify(token, secret, {maxAge: expiration});
+        
+        try{
+            const {data} = jwt.verify(
+                token,
+                secret,
+                {maxAge: expiration}
+            );
             req.user = data;
-        } catch {
+        }catch{
             console.log('Invalid Token');
         }
 
         return req;
     },
-    signToken: function({ username, email, _id}) {
-        const payload = { username, email, _id};
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+
+    signToken({username, email, _id}){
+        const payload = {username, email, _id};
+        return jwt.sign(
+            {data: payload},
+            secret,
+            {expiresIn: expiration}
+        );
     }
 };
