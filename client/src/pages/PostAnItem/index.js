ee0038f79ce60e015b4239e2edaec48cbee02232
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-11-16 00:00:20 -0500

    PostAnItem styling

--- client/src/pages/PostAnItem/index.js
+++ client/src/pages/PostAnItem/index.js
@@ -1,6 +1,8 @@
 
-// IMPORT
+// IMPORTS
 import {useState} from "react";
+
+import './index.css';
 
 
 // COMPONENT
@@ -36,34 +38,36 @@ export default function PostAnItem(){
     }
 
     return <>
-        <form id='post-an-item-form' onSubmit={handleSubmit}>
-            <div className="form-floating">
-                <input
-                    className="form-control"
-                    value={newPostData.title}
-                    onChange={handleChange}
-                    id="new-post-name"
-                    name="title"
-                    placeholder="Enter title"
-                    required
-                />
-                <label htmlFor="new-post-title">Enter title</label>
-            </div>
+        <div className="container-fluid">
+            <form id='post-an-item-form' className="d-flex flex-column align-items-center mt-3" onSubmit={handleSubmit}>
+                <div className="form-floating mb-4">
+                    <input
+                        className="form-control"
+                        value={newPostData.title}
+                        onChange={handleChange}
+                        id="new-post-name"
+                        name="title"
+                        placeholder="Enter title"
+                        required
+                    />
+                    <label htmlFor="new-post-title">Enter title</label>
+                </div>
 
-            <div className="form-floating">
-                <textarea
-                    className="form-control"
-                    value={newPostData.description}
-                    onChange={handleChange}
-                    id="new-post-description"
-                    name="description"
-                    placeholder="Enter description"
-                    style={{height: '200px'}}
-                    required />
-                <label htmlFor="new-post-description">Enter description</label>
-            </div>
+                <div className="form-floating mb-4">
+                    <textarea
+                        className="form-control"
+                        value={newPostData.description}
+                        onChange={handleChange}
+                        id="new-post-description"
+                        name="description"
+                        placeholder="Enter description"
+                        style={{height: '200px'}}
+                        required />
+                    <label htmlFor="new-post-description">Enter description</label>
+                </div>
 
-            <button className="btn btn-primary" type="submit" form="post-an-item-form">Post</button>
-        </form>
+                <button className="btn submit-btn hover-opacity" type="submit" form="post-an-item-form">Post</button>
+            </form>
+        </div>
     </>;
 }
