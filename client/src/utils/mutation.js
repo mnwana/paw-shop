f4e5761b50edf8b407a83cc72042c2ce2243157d
mnwana <mariellenwana@gmail.com>
2022-11-16 21:18:01 -0500

    add mutatinos on client side

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
@@ -13,8 +13,18 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!,  $borough: String!) {
-    addUser(username: $username, email: $email, password: $password, borough: $borough) {
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+    $borough: String!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+      borough: $borough
+    ) {
       token
       user {
         _id
@@ -25,8 +35,18 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_POST = gql`
-  mutation addPost($postText: String!, $animalType: String!, $category: String!, $condition: String!) {
-    addPost(postText: $postText, animalType: $animalType, category: $category, condition: $condition) {
+  mutation addPost(
+    $postText: String!
+    $animalType: String!
+    $category: String!
+    $condition: String!
+  ) {
+    addPost(
+      postText: $postText
+      animalType: $animalType
+      category: $category
+      condition: $condition
+    ) {
       _id
       postText
       createdAt
@@ -71,10 +91,108 @@ export const ADD_COMMENT = gql`
   }
 `;
 
-
 export const ADD_REPLY = gql`
-  mutation addReply($commentId: ID!,($postId: ID! ,$replyBody: String!) {
+  mutation addReply($commentId: ID!, $postId: ID!, $replyBody: String!) {
     addReply(commentId: $commentId, postId: $postId, replyBody: $replyBody) {
+      _id
+      postText
+      createdAt
+      username
+      animalType
+      condition
+      category
+      userId
+      commentCount
+      watchingCount
+      watching {
+        _id
+      }
+      comments {
+        _id
+        commentBody
+        updatedAt
+        createdAt
+        userId
+        replies {
+          _id
+          replyBody
+          createdAt
+          userId
+        }
+      }
+    }
+  }
+`;
+
+export const DELETE_COMMENT = gql`
+  mutation deletecomment($postId: ID!, $commentId: ID!) {
+    deletecomment(postId: $postId, commentId: $commentId) {
+      _id
+      postText
+      createdAt
+      username
+      animalType
+      condition
+      category
+      userId
+      commentCount
+      watchingCount
+      watching {
+        _id
+      }
+      comments {
+        _id
+        commentBody
+        updatedAt
+        createdAt
+        userId
+        replies {
+          _id
+          replyBody
+          createdAt
+          userId
+        }
+      }
+    }
+  }
+`;
+
+export const DELETE_REPLY = gql`
+  mutation deleteReply($commentId: ID!, $postId: ID!, $replyId: ID!) {
+    deleteReply(commentId: $commentId, postId: $postId, replyId: $replyId) {
+      _id
+      postText
+      createdAt
+      username
+      animalType
+      condition
+      category
+      userId
+      commentCount
+      watchingCount
+      watching {
+        _id
+      }
+      comments {
+        _id
+        commentBody
+        updatedAt
+        createdAt
+        userId
+        replies {
+          _id
+          replyBody
+          createdAt
+          userId
+        }
+      }
+    }
+  }
+`;
+
+export const UPDATE_POST = gql`
+  updatePost($postId: ID!, $postText: String!, $animalType: String!, $category: String!, $condition: String!) {
+    updatePost(postId: $postId, postText: $postText, animalType: $animalType, category: $category, condition: $condition) {
       _id
       postText
       createdAt
